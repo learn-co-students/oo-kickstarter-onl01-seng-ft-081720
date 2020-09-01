c24bb6a6b425abe8d3b2b98456f5a10d66053ebd
Richard Kurwitz <kurwitz3@aol.com>
2020-09-01 20:55:06 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Project
+  attr_accessor  :backer
+attr_reader :title,:backers
+
+def initialize(title)
+  @title = title
+   @backers =[]
+end 
+
+def add_backer(backer)
+  @backers <<  backer 
+  backer.back_project(self)
+ end
+end 
