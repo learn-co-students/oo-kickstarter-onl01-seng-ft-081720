bd65c0249c52c75e94f57c9b37129800f9ac480c
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-01 01:16:58 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project 
+  attr_accessor :title, :backers 
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(backer) 
+    @backers << backer 
+    backers.backed_project << self
+  end 
+end 
